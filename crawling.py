340de7c8c96751e763e5567d15a58e7217a70089
Lumizz <yiansh15@gmail.com>
2020-04-08 21:53:12 +1000

    bug fixed

--- crawling.py
+++ crawling.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
+# In[1]:
 
 
 from selenium import webdriver
@@ -12,8 +12,7 @@ from linebot.models import TextSendMessage
 import json
 
 
-# In[20]:
-
+# In[27]:
 
 
 options = FirefoxOptions()
@@ -26,37 +25,47 @@ driver.close()
 soup = BeautifulSoup(htmltext, 'html.parser')
 deals_divs = soup.findAll("div", {"class": "node-ozbdeal"})
 
-line_bot_api = LineBotApi('Your_token')
+line_bot_api = LineBotApi('your_token')
 
 
 new_deals = []
 update = False
 
 with open('data.txt') as json_file:
-    current_data = json.load(json_file)
-    pre_data = [p['id'] for p in current_data['deals']]
+    pre_data = json.load(json_file)
+    tmp_data = [p['id'] for p in pre_data['deals']]
 
-for deal in deals_divs:
-    
-    h2 = deal.find('h2')
-    deal_id = h2['id'].replace('title','')
-    if deal_id not in pre_data:
-        update = True
-        new_deals.append(deal_id)
-        title = h2['data-title']
-        
-        #push message to one user
-        line_bot_api.push_message(
-            'Your_user_id', 
-            TextSendMessage(text=title+" https://www.ozbargain.com.au/node/"+deal_id)
-        )
+for i,deal in enumerate(deals_divs):
+    if i >= 10:
+        break
+    else:
+        h2 = deal.find('h2')
+        deal_id = h2['id'].replace('title','')
+
+        if deal_id not in tmp_data:
+            update = True
+            new_deals.append(deal_id)
+            #title = h2['data-title']
+
+            #push message to one user
+            line_bot_api.push_message(
+                'user_id', 
+                TextSendMessage(text=title+" https://www.ozbargain.com.au/node/"+deal_id)
+            )
         
 if update:
     for deal in new_deals:
-        current_data['deals'].pop()
-        current_data['deals'].insert(0,{'id':deal})
+        pre_data['deals'].insert(0,{'id':deal})
+        pre_data['deals'].pop()
     
     with open('data.txt', 'w') as outfile:
-        json.dump(current_data, outfile)
+        json.dump(pre_data, outfile)
         
+        
+
+
+# In[ ]:
+
+
+
 
